6cf15bf4ec3e221fc92d23a488b377e07c80f9fd
liwuhui-cheng <1652853513@qq.com>
2019-12-24 20:57:04 +0800

    修改提交

---
+++ src/main/java/com/lixuecheng/test/Study.java
@@ -0,0 +1,21 @@
+package com.lixuecheng.test;
+
+public class Study {
+
+	/**
+	 * 判断输入的数据是否是奇数
+	 * 奇数返回true
+	 * @param num
+	 * @return
+	 */
+	public static Boolean isOddNumber(int num) {
+		Boolean flag = null;
+		if (num % 2 == 0) {
+			flag = false;
+		}else {
+		flag = true;
+		}
+		return flag;
+	}
+	
+}
